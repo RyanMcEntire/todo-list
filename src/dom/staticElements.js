fa290a98e7683c64e344b6ca4ff9cecd2f0733ed
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-03-16 19:04:25 -0600

    fix html rendering issues and install webpack html plugin

--- src/dom/staticElements.js
+++ src/dom/staticElements.js
@@ -5,7 +5,7 @@ function makeHeader() {
   return new Element('div')
     .addAttributes({ class: 'header', id: 'header' })
     .addChild(
-      new Element('div').addAttributes({ class: 'sidebar', id: 'sidebar' })
+      new Element('div').addAttributes({ class: 'projectFormContainer' })
     )
     .build();
 }
@@ -13,82 +13,81 @@ function makeHeader() {
 function makeContent() {
   return new Element('div')
     .addAttributes({ class: 'content', id: 'content' })
-    .build();
-}
-
-function makeSidebar() {
-  return new Element('div')
-    .addAttributes({ class: 'sidebar', id: 'sidebar' })
     .addChild(
-      new Element('div').addAttributes({ class: 'sortManagerContainer' })
-    )
-    .addChild(new Element('div').addAttributes({ class: 'borderDiv' }))
-    .addChild(
-      new Element('div')
-        .addAttributes({ class: 'newProjectFooter' })
+      new Element('div') // sidebar
+        .addAttributes({ class: 'sidebar', id: 'sidebar' })
+        .addChild(
+          new Element('div').addAttributes({ class: 'sortManagerContainer' })
+        )
+        .addChild(new Element('div').addAttributes({ class: 'borderDiv' }))
         .addChild(
           new Element('div')
-            .addAttributes({ class: 'createNewProjectLabel' })
-            .addText('New Project')
+            .addAttributes({ class: 'newProjectFooter' })
+            .addChild(
+              new Element('div')
+                .addAttributes({ class: 'createNewProjectLabel' })
+                .addText('New Project')
+            )
+            .addChild(
+              new Element('button')
+                .addAttributes({
+                  class: 'callNewProjectModal',
+                  id: 'callNewProjectModal',
+                })
+                .addText('+')
+                .addEventListener('click', (e) => processClick(e))
+            )
+        )
+    )
+    .addChild(
+      new Element('div')
+        .addAttributes({ class: 'contentArea', id: 'contentArea' })
+        .addChild(
+          new Element('div')
+            .addAttributes({
+              class: 'taskInfoArea',
+            })
+            .addChild(new Element('h2').addText('Will Be Dynamic Eventually'))
+            .addChild(
+              new Element('button')
+                .addAttributes({
+                  class: 'newTask',
+                  id: 'newTask',
+                })
+                .addText('New Task +')
+                .addEventListener('submit', (e) => processClick(e))
+            )
+        )
+
+        .addChild(
+          new Element('div').addAttributes({
+            id: 'taskView',
+          })
+        )
+        .addChild(
+          new Element('div').addAttributes({
+            class: 'borderDiv',
+          })
+        )
+        .addChild(
+          new Element('div').addAttributes({
+            id: 'taskFormContainer',
+          })
         )
         .addChild(
           new Element('button')
             .addAttributes({
-              class: 'callNewProjectModal',
-              id: 'callNewProjectModal',
+              class: 'consoleTable',
+              id: 'consoleTable',
             })
-            .addText('+')
+            .addText('console.table()')
             .addEventListener('click', (e) => processClick(e))
         )
     )
     .build();
 }
 
-function makeContentArea() {
-  return new Element('div')
-    .addAttributes({ class: 'contentArea', id: 'contentArea' })
-    .addChild(
-      new Element('div')
-        .addAttributes({
-          class: 'taskInfoArea',
-        })
-        .addChild(new Element('h2').addText('Will Be Dynamic Eventually'))
-    )
-    .addChild(
-      new Element('button')
-        .addAttributes({
-          class: 'newTask',
-          id: 'newTask',
-        })
-        .addText('New Task +')
-        .addEventListener('submit', (e) => processClick(e))
-    )
-    .addChild(
-      new Element('div').addAttributes({
-        id: 'taskView',
-      })
-    )
-    .addChild(
-      new Element('div').addAttributes({
-        class: 'borderDiv',
-      })
-    )
-    .addChild(
-      new Element('div').addAttributes({
-        id: 'taskFormContainer',
-      })
-    )
-    .addChild(
-      new Element('button')
-        .addAttributes({
-          class: 'consoleTable',
-          id: 'consoleTable',
-        })
-        .addText('console.table()')
-        .addEventListener('click', (e) => processClick(e))
-    )
-    .build();
-}
+
 
 const isValidElement = (element) => {
   return element instanceof HTMLElement;
@@ -98,8 +97,7 @@ const initializePage = () => {
   const elementTreeList = [
     makeHeader(),
     makeContent(),
-    makeSidebar(),
-    makeContentArea(),
+
   ];
 
   elementTreeList.forEach((tree) => {
